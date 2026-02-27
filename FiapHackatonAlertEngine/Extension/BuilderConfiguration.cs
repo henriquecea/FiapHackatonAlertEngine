bd51859802600ac5e35e9522f2641dc8dff4e4d1
Henrique Cea <henrique.cea@peakinvest.com.br>
2026-02-26 21:19:52 -0300

    start the implementation of database

---
+++ FiapHackatonAlertEngine/Extension/BuilderConfiguration.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace FiapHackatonAlertEngine.WebAPI.Extension;
+
+public static class BuilderConfiguration
+{
+    public static void AddDbContext(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    }
+
+    public static void AddRabbitMQ(this WebApplicationBuilder builder)
+    {
+        builder.Services.Configure<RabbitMqSettings>(
+            builder.Configuration.GetSection("RabbitMq"));
+
+        builder.Services.AddSingleton<RabbitMQ.Client.IConnectionFactory>(sp =>
+        {
+            var settings = sp.GetRequiredService<IOptions<RabbitMqSettings>>().Value;
+
+            return new ConnectionFactory
+            {
+                HostName = settings.Host,
+                Port = settings.Port,
+                UserName = settings.Username,
+                Password = settings.Password
+            };
+        });
+
+        builder.Services.AddSingleton<IRabbitMQService, RabbitMQService>();
+
+        builder.Services.AddScoped<IRabbitMQReceiver, RabbitMQReceiver>();
+        builder.Services.AddScoped<IRabbitMQMessageHandler, SimulationHandler>();
+
+        builder.Services.AddHostedService<RabbitMQConsumer>();
+    }
+
+    public static void AddSwagger(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Fiap Hackaton - Simulation", Version = "v1" });
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
+    }
+
+    public static void AddAuthWithJWT(this WebApplicationBuilder builder)
+    {
+        var secretKey = builder.Configuration["JWT:SecretKey"]
+                      ?? throw new InvalidOperationException("JWT Secret Key is not configured.");
+
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ClockSkew = TimeSpan.Zero,
+                    ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+                    ValidAudience = builder.Configuration["JWT:ValidAudience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+                };
+            });
+    }
+}
--- FiapHackatonAlertEngine/Program.cs
+++ FiapHackatonAlertEngine/Program.cs
@@ -1,17 +1,38 @@
+using FiapHackatonAlertEngine.WebAPI.Extension;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.AddSwagger();
 
-builder.Services.AddControllers();
+builder.Services.AddAuthorization();
+builder.Services.AddControllers()
+                .AddNewtonsoftJson();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    // Aplicar migrações pendentes ao iniciar a aplicações
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 
+// Redirecionamento da raiz para /swagger
+app.MapGet("/", context =>
+{
+    context.Response.Redirect("/swagger");
+    return Task.CompletedTask;
+});
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+await app.RunAsync();
